3e9e9a51d63a1f0a838d5a483d622d16f862661c
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-05-13 19:55:48 +0530

    feat: add social media links with icons to the homepage for enhanced connectivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,46 @@
 import AllBlogs from "@/components/AllBlogs";
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+
+type LinkType = {
+	label: string;
+	href: string;
+};
+
+const links: LinkType[] = [
+	{
+		label: "twitter",
+		href: "https://x.com/inclinedadarsh",
+	},
+	{
+		label: "github",
+		href: "https://github.com/inclinedadarsh",
+	},
+	{
+		label: "linkedin",
+		href: "https://linkedin.com/in/dubeyadarsh/",
+	},
+];
 
 export default function Home() {
 	return (
 		<main className="md:mt-5">
 			<h1 className="text-2xl font-bold font-title">Adarsh Dubey</h1>
-			<p className="mt-6 text-foreground/80 mb-5 md:mb-10">
+			<div className="mt-2 text-foreground/80 flex gap-4">
+				{links.map(link => (
+					<Link
+						key={link.label}
+						href={link.href}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="flex gap-1 items-center hover:text-foreground/60 transition-colors hover:cursor-pointer"
+					>
+						<ArrowUpRight size={16} />
+						{link.label}
+					</Link>
+				))}
+			</div>
+			<p className="mt-4 text-foreground/80 mb-5 md:mb-10">
 				I'm a Vim enthusiast and tab advocate, finding unmatched
 				efficiency in Vim's keystroke commands and tabs' flexibility for
 				personal viewing preferences. This extends to my support for
