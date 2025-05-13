a7e2e66cc08373c1d56ebabd2d307bcc78359a65
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-05-13 16:42:53 +0530

    feat: add Navbar component and integrate it into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({
 	variable: "--font-inter",
@@ -19,7 +20,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className} antialiased`}>{children}</body>
+			<body
+				className={`${inter.className} antialiased max-w-xl px-5 md:px-0 mx-auto`}
+			>
+				<Navbar />
+				{children}
+			</body>
 		</html>
 	);
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+type NavLink = {
+	label: string;
+	href: string;
+	openInNewTab?: boolean;
+};
+
+const navLinks: NavLink[] = [
+	{
+		label: "home",
+		href: "/",
+	},
+	{
+		label: "blogs",
+		href: "/blogs",
+	},
+	{
+		label: "website",
+		href: "https://adarshdubey.com",
+		openInNewTab: true,
+	},
+];
+
+const Navbar = () => {
+	return (
+		<nav id="navbar" className="py-5 md:py-10">
+			<ul className="flex gap-5 md:gap-7">
+				{navLinks.map(link => (
+					<li key={link.label}>
+						<Link
+							className={cn(
+								"text-primary hover:text-primary/70 transition-colors",
+							)}
+							href={link.href}
+							target={link.openInNewTab ? "_blank" : "_self"}
+							rel={link.openInNewTab ? "noopener noreferrer" : ""}
+						>
+							{link.label}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
