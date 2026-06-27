a479f2705ae9cddf20e3739df6d635d9b26a8ad8
Adarsh Dubey <dubeyadarshmain@gmail.com>
2026-06-27 16:44:33 +0530

    fix: make footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,11 +6,11 @@ const Footer = () => {
 	return (
 		<footer
 			id="footer"
-			className="mt-10 md:mt-16 py-8 border-t border-border flex items-center justify-between"
+			className="mt-10 md:mt-16 py-8 border-t border-border flex flex-col md:flex-row md:items-center justify-between gap-4"
 		>
 			<Link
 				href="mailto:dubeyadarshmain@gmail.com"
-				className="text-foreground-title px-2 py-1 border border-border rounded-sm leading-[150%] hover:border-foreground-body/40 transition-colors"
+				className="text-foreground-title px-2 py-1 border border-border rounded-sm leading-[150%] hover:border-foreground-body/40 transition-colors w-fit"
 			>
 				Let's talk?
 			</Link>
